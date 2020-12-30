80a3255bb7bbec12f2bdc231096b58cb673847a9
SoUuzaaa <felipebrasilsantos@outlook.com>
2020-12-30 13:11:20 -0300

    Finish script of counter.html and the Page

--- js/script.js
+++ js/script.js
@@ -1,11 +1,28 @@
 function timeLeft() {
-    let dateInput = new Date(document.querySelector("#dateNow").value)
+    let timeStorage = new Date(JSON.parse(sessionStorage.getItem('dateInput')))
     let currentDate = new Date()
-    let differece = dateInput.getTime() - currentDate.getTime()
+    var differece = timeStorage.getTime() - currentDate.getTime()
 
     let days = Math.floor((differece / 1000 / 60 / 60 / 24))
     let hrs = Math.floor((differece / 1000 / 60 / 60) % 24)
     let min = Math.floor((differece / 1000 / 60) % 60)
     let secs = Math.floor((differece / 1000) % 60)
-    document.getElementById("writeHere").innerHTML = secs
+    
+    document.getElementById("days").innerHTML = days
+    document.getElementById("hours").innerHTML = hrs
+    document.getElementById("minutes").innerHTML = min
+    document.getElementById("secs").innerHTML = secs
+    if(differece < 0) {
+        clearInterval(timeLeft)
+
+        document.getElementById("days").innerHTML = "00"
+        document.getElementById("hours").innerHTML = "00"
+        document.getElementById("minutes").innerHTML = "00"
+        document.getElementById("secs").innerHTML = "00"
+    }
 }
+setInterval(timeLeft, 1000)
+
+document.getElementById("btnRestart").addEventListener("click", function returnPage() {
+    window.location = "index.html"
+})
